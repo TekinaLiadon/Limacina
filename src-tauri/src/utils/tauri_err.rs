6d637812ce40e0dcd71b1a895d2ad4958b524dbd
Tekina <nm0321@yandex.ru>
2026-05-17 16:54:43 +0300

    Изменение запуска ванильного майнкрафта

--- src-tauri/src/utils/tauri_err.rs
+++ src-tauri/src/utils/tauri_err.rs
@@ -1,9 +1,11 @@
+use crate::log_err;
 
 #[derive(Debug, serde::Serialize)]
 pub struct CommandError(String);
 
 impl From<anyhow::Error> for CommandError {
     fn from(error: anyhow::Error) -> Self {
+        log_err!("{:#}", error);
         Self(format!("{:#}", error))
     }
 }
